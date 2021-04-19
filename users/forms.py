253b154a207207c191c00f88b8d3bac5c1bb9554
rdotjain <rupanshijain45678@gmail.com>
2021-04-19 19:20:40 +0530

    Login/Register functional

---
+++ users/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# from phonenumber_field import modelfields
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    phone = forms.IntegerField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone', 'password1', 'password2']
+
+    def save(self, *args, **kwargs):
+        super(UserRegistrationForm, self).save(*args, **kwargs)
