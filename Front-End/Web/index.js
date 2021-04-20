565686441382540340c0acbb8c1fd26bf79f708e
yash22arora <yash22arora@gmail.com>
2021-04-20 09:44:16 +0530

    Tried adding flip animation
    Need to fix some stuff

--- Front-End/Web/index.js
+++ Front-End/Web/index.js
@@ -1,12 +1,20 @@
 function showAbout()
 {
     
+    // var k = 180;
+    // document.querySelector("#title").style.transform = "rotatey(" + k + "deg)";
+    // document.querySelector("#title").style.transitionDuration = "0.5s"
+
     document.querySelector("#title").classList.add("hidden");
     document.querySelector("#about").classList.remove("hidden");
 }
 
 function showHome()
 {
+    // var k = 0;
+    // document.querySelector("#title").style.transform = "rotatey(" + k + "deg)";
+    // document.querySelector("#title").style.transitionDuration = "0.5s"
+    
     document.querySelector("#title").classList.remove("hidden");
     document.querySelector("#about").classList.add("hidden");
 }
