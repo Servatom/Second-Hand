1939b4086059fc6d013ca999b64c68e8256826af
rdotjain <rupanshijain45678@gmail.com>
2021-05-02 15:06:23 +0530

    Added mobile regex

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import RegexValidator
 
-# from phonenumber_field import modelfields
-
+MOBILE_REGEX = "^(\+\d{1,3}[- ]?)?\d{10}$"
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
-    phone = forms.IntegerField()
+    phone = forms.IntegerField(
+        required=True, 
+        validators=[
+            RegexValidator(
+                regex=MOBILE_REGEX,
+                message="Enter a valid mobile number",
+                code="invalid_mobile",
+            )
+        ]
+    )
     class Meta:
         model = User
         fields = ['username', 'phone', 'email', 'password1', 'password2']
