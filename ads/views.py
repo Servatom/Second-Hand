96144e6243fc12252012312563385870aa150825
rdotjain <rupanshijain45678@gmail.com>
2021-05-10 05:57:35 +0530

    updated view permissions

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -15,7 +16,8 @@ class PostListView(ListView):
     template_name = 'ads/ads.html'
     context_object_name = 'posts'
     
-class PostCreateView(CreateView):
+
+class PostCreateView(LoginRequiredMixin,CreateView):
     model = Post
     fields = ['title', 'description', 'price', 'image']
 
@@ -23,20 +25,36 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostDetailView(DetailView):
+
+class PostDetailView(LoginRequiredMixin,DetailView):
     model = Post
 
-class PostUpdateView(UpdateView):
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Post
     fields = ['title', 'description', 'price', 'image']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
-class PostDeleteView(DeleteView):
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Post
     success_url = reverse_lazy('ads-list')
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
 
 def index(request):
     return render(request, 'ads/index.html')
