409b5d9ca24206798029c21c7ca1e6a90cfce766
rdotjain <rupanshijain45678@gmail.com>
2021-05-14 02:47:41 +0530

    profile

--- users/models.py
+++ users/models.py
@@ -48,7 +48,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
+        if img.height > 500 or img.width > 500:
+            output_size = (400, 400)
             img.thumbnail(output_size)
             img.save(self.image.path)
